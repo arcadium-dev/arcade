5b4f711c7abbca583087c97c4d3696ecfd91059b
Ian Cahoon <167669+icahoon@users.noreply.github.com>
2024-08-13 00:13:49 -0600

    update copyright

--- asset/item.go
+++ asset/item.go
@@ -1,4 +1,4 @@
-//  Copyright 2022-2023 arcadium.dev <info@arcadium.dev>
+//  Copyright 2022-2024 arcadium.dev <info@arcadium.dev>
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -17,8 +17,9 @@ package asset // import "arcadium.dev/arcade/asset"
 import (
 	"database/sql/driver"
 
-	"arcadium.dev/arcade"
 	"github.com/google/uuid"
+
+	"arcadium.dev/arcade"
 )
 
 const (

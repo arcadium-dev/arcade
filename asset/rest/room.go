9dcc1f0ad6a1edaf567f36fe17d1419bbbda6098
Ian Cahoon <167669+icahoon@users.noreply.github.com>
2024-08-15 00:31:14 -0600

    fix import paths (#100)

--- asset/rest/room.go
+++ asset/rest/room.go
@@ -14,7 +14,9 @@
 
 package rest // import "arcadium.dev/arcade/asset/rest"
 
-import "arcadium.dev/arcade"
+import (
+	"arcadium.dev/arcade"
+)
 
 type (
 	// RoomCreateRequest is used to request an room be created.
